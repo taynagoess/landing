7812d8c2988cb21eecb9a663d10dd68062224b01
Tayná Goes <145618162+taynagoess@users.noreply.github.com>
2025-12-21 15:41:39 -0300

    Change slidesToShow from 2 to 1 at 1024px breakpoint

--- assets/js/app.js
+++ assets/js/app.js
@@ -356,7 +356,7 @@ $(function () {
       swipe: true,
       touchMove: true,
       responsive: [
-        { breakpoint: 1024, settings: { slidesToShow: 2 } },
+        { breakpoint: 1024, settings: { slidesToShow: 1 } },
         { breakpoint: 768, settings: { slidesToShow: 1 } }
       ]
     });
